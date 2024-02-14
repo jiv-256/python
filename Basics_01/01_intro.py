1317b2a8aaf12bcf2be1df27edc20d54b8c451d8
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-02-14 09:31:51 +0530

    Add files via upload

---
+++ Basics_01/01_intro.py
@@ -0,0 +1,29 @@
+# Comments
+# single line comment
+# rough type copy - 5 (200 pages)
+# hanger 
+# lunch box
+# ja na 
+# multi line comment
+''' 
+author : Harry
+licenced to : ABC company 
+'''
+# note:- if you use triple coute alone then it is  in print then it become string literal 
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up abobe the world so high,
+Like a daimond in the sky.''')
+
+'''
+output :
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up abobe the world so high,
+Like a daimond in the sky.
+'''
+
+# lets install an external module and use it to perform an operation of your interest
+# first install play sound by writing a command "pip install playsound" in terminal
+from playsound import playsound
+playsound('F:\\python\\Hamare sath shree raghunath.mp3')
