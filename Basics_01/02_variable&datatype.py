e092f7cdfaa7f7f85ceb1ae20f67214097255bc4
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-02-15 10:51:01 +0530

    Add files via upload

---
+++ Basics_01/02_variable&datatype.py
@@ -0,0 +1,32 @@
+# A variable is the name given to a memory location in a program
+# variable - container to store a value
+a = "rajan" 
+b = 356
+c = 45.43
+# Python is dynamically typed, which means that variable types are determined and checked at runtime rather than during compilation. In dynamically typed languages like Python, you don't need to explicitly declare the variable type before using it.
+
+# keywords - reserved words in python
+'''
+False    	def	       if           raise       None    	del	       import	    return
+True	    elif	   in	        try         and	        else	   is	        while
+as	        except	   lambda	    with        assert	    finally	   nonlocal	    yield
+break	    for	       not          class	    from	   or	       continue	    global	   
+pass
+
+'''
+# datatype 
+a = "rajiv" #string
+b = 356 #integer
+c = 45.43 #float
+d = True #boolean
+e = None #none
+
+print(e)
+# output- None
+
+print(type(a))
+# output- <class 'str'>
+# identifier - class/function/variable name
+# ->Python Identifier is the name we give to identify a variable, function, class, module or other object.
+
+
