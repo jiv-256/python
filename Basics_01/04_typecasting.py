9bf0e1a1bc769aa7d61624250ed1a3781cf9b1d1
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-02-16 00:34:06 +0530

    Add files via upload

---
+++ Basics_01/04_typecasting.py
@@ -0,0 +1,16 @@
+a = "345"
+# the type function used to find the type of datatype of a variable
+print(type(a))
+# output -> <class 'str'>
+
+# typecasting - Type Casting is the method to convert the Python variable datatype into a certain data type in order to perform the required operation by users.
+
+a = int(a)
+print(a) #output - 345
+print(type(a)) #output - <class 'int'>
+
+# note => A number can be converted in a string and vice-versa
+b = 34
+b = str(b)
+print(b) #output=>34
+print(type(b)) #output=><class 'str'>
