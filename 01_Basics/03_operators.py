72f65136722122d0d29ad803e6e024b1079c91d5
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-02-24 00:54:33 +0530

    Add files via upload

---
+++ 01_Basics/03_operators.py
@@ -0,0 +1,12 @@
+'''
+There are seven different categories of operators in Python: 
+1. arithmetic -> (+ , - , * , / )
+2. assignment -> (= , += , -= )
+3. comparison -> (== , > , < , >= , <= , != )
+4. logical/boolean operators -> (and , or , not)
+5. membership -> (in , not in)
+7. identity  -> ()
+'''
+
+
+
