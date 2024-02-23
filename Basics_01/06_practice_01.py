c8aa7b0db257655768c10fb73caaf7093c324cd0
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-02-23 16:29:37 +0530

    Delete Basics_01 directory

--- Basics_01/06_practice_01.py
+++
@@ -1,8 +0,0 @@
-# Adding two no 
-a = 234
-b = 4
-print("the sum of a and b is = ", a+b)
-# output ->the sum of a and b is =  238
-
-# find remainder
-print("the remainder when a is divided by b is = ", a%b)
