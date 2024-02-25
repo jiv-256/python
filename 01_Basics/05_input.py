8c4a07822a5012e7d46c7bc7b18e3f76cd7585af
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-02-25 23:04:36 +0530

    Add files via upload

---
+++ 01_Basics/05_input.py
@@ -0,0 +1,11 @@
+a = input("Enter your name: ")
+print(a) 
+print(type(a)) #output -> <class 'str'>
+
+'''
+note : Everytime you use this input funtion it allows user to take input from the keyboard as a string and the output is always a string if you have to take a integer as an input so you have to convert it by typecasting
+'''
+b = input("Enter your AGE: ")
+b = int(b) # Convert b to an Integer(if possible)
+print(b) 
+print(type(b)) #output -> <class 'int'>
