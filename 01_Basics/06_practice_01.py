1c884a56ea470678bf6899ef3083531c565690da
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-02-25 23:07:37 +0530

    Add files via upload

---
+++ 01_Basics/06_practice_01.py
@@ -0,0 +1,8 @@
+# Adding two no 
+a = 234
+b = 4
+print("the sum of a and b is = ", a+b)
+# output ->the sum of a and b is =  238
+
+# find remainder
+print("the remainder when a is divided by b is = ", a%b)
